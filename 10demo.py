b56d9b72ee834b8ee4a100863ced6466837a4b89
SaiTheGuy1413 <sainathp2016@gmail.com>
2026-05-17 17:38:01 -0700

    update2

--- 10demo.py
+++ 10demo.py
@@ -14,3 +14,22 @@ print(math.log(2))
 #print(1 / 0)           # divide by zero error
 #print(math.log(0))     # math domain error
 #print(math.sqrt(-1))   # math domain error
+
+a = 3                       # side of triangle
+b = 4                       # side of triangle
+c = math.sqrt(a**2 + b**2)  # hypotenuse
+print(c)
+print(type(a), type(b), type(c), sep=', ', end='!\n')
+
+def pythagoras(a,b):
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+hyp = pythagoras(3,4)
+print(hyp)
+
+#simpler version
+def hypontenuse(a,b):
+    return math.sqrt(a**2 + b**2)
+
+print (hypontenuse(3,4))
