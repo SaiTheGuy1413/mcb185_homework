0d3405044477bb4919d8d846d0d969365110c6d2
SaiTheGuy1413 <sainathp2016@gmail.com>
2026-05-27 21:25:22 -0700

    update

---
+++ 36countgff.py
@@ -0,0 +1,15 @@
+import sys
+import mcb185
+import gzip
+
+count = {}
+with gzip.open(sys.argv[1], 'rt') as fp:
+    for line in fp:
+        if line.startswith('#'): continue
+        f = line.split()
+        feature = f[2]
+        if feature not in count: count[feature] = 0
+        count[feature] += 1
+    for f, n in count.items(): print(f, n)
+
+    for k in sorted(count): print(k, count[k])
